e8cae440eb43ce3438b223cd49b846123fd41d11
dieghernan <diego.hernangomezherrero@gmail.com>
2024-04-03 10:59:09 +0000

    Update manual and pkgdown

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -1,9 +1,9 @@
 list(
   rd_family_title = list(
     lookup = "Address Lookup API: ",
-    geocoding = "Geocoding strings: ",
+    geocoding = "Geocoding: ",
     reverse = "Reverse geocoding coordinates: ",
-    spatial = "Get \\CRANpkg{sf} objects: ",
+    spatial = "Get \\code{\\link[sf:sf]{sf}} objects: ",
     api_management = "API Management: "
   )
 )
