f07efb9a3011e1e1eb07b2350a064cedcb425aa0
Diego H. <dieghernan@users.noreply.github.com>
2026-06-17 23:55:15 +0200

    Double trail

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -1,10 +1,10 @@
 list(
   rd_family_title = list(
-    lookup = "Address lookup functions",
-    geocoding = "Geocoding functions",
-    reverse = "Reverse geocoding functions",
-    spatial = "Spatial output functions",
-    amenity = "Amenity lookup functions",
-    api_management = "API management functions"
+    lookup = "Address lookup functions:",
+    geocoding = "Geocoding functions:",
+    reverse = "Reverse geocoding functions:",
+    spatial = "Spatial output functions:",
+    amenity = "Amenity lookup functions:",
+    api_management = "API management functions:"
   )
 )
