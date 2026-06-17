4fe97167f2b21415c9d9c99471f9e89930392d5d
Diego H. <dieghernan@users.noreply.github.com>
2026-06-17 23:52:09 +0200

    Need to redocument

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -1,10 +1,10 @@
 list(
   rd_family_title = list(
-    lookup = "Address lookup: ",
-    geocoding = "Geocoding: ",
-    reverse = "Reverse geocoding: ",
-    spatial = "\\code{\\link[sf:sf]{sf}} outputs: ",
-    amenity = "Amenity lookup: ",
-    api_management = "API management: "
+    lookup = "Address lookup functions",
+    geocoding = "Geocoding functions",
+    reverse = "Reverse geocoding functions",
+    spatial = "Spatial output functions",
+    amenity = "Amenity lookup functions",
+    api_management = "API management functions"
   )
 )
